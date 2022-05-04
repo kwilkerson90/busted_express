417e9ba25299ffba112d3b365a966e2f71bacb77
Kendra Wilkerson <kwilkerson@ggc.edu>
2022-05-04 10:57:02 -0400

    modified port number and case of port/hostname

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const hostname = '127.0.0.1';
-const port = 3333;
+const port = 3000;
 
 const express = require('express'),
     es6Renderer = require('express-es6-template-engine');
@@ -16,7 +16,7 @@ app.set('view engine', 'html');
 
 const server = http.createServer(app);
 
-server.listen(PORT, HOSTNAME, () => {
+server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}`)
 });
 
